1960e8cfd0888eb7115c0dcd63f8b2e8b2ec955b
Simin Kou <132246826+SiminKoux@users.noreply.github.com>
2025-03-12 13:11:15 +1300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,31 +2,31 @@
 # change the corresponding .txt name
 # For example, if train for indoor scene 'Lab' in werp grids, 
 # please let the 'common_werp_recon_indoor.txt' renamed into 'common.txt' 
-python main.py --config configs/omni/lab/default.txt 
+python main.py --config configs/DyOmni/Lab/default.txt 
 
 # Only Evaluation and Inference Reconstruction Frames based on the stage 1's model
 # change the corresponding .txt name
 # For example, if train for indoor scene 'Lab' in werp grids, 
 # please let the 'common_werp_recon_indoor.txt' renamed into 'common.txt' 
-python main.py --config configs/omni/lab/default.txt --evaluation 1
+python main.py --config configs/DyOmni/Lab/default.txt --evaluation 1
 
 # Extract initialized palette based on the stage 1's model
 # change the corresponding .txt name
 # For example, if train for indoor scene 'Lab' in werp grids, 
 # please let the 'common_werp_recon_indoor.txt' renamed into 'common.txt' 
-python main.py --config configs/omni/lab/default.txt --palette_extract 1
+python main.py --config configs/DyOmni/Lab/default.txt --palette_extract 1
 
 # Stabilization based on the stage 1's model 
 # change the corresponding .txt name
 # For example, if train for indoor scene 'Lab' in werp grids, 
 # please let the 'common_werp_stabilize_indoor.txt' renamed into 'common.txt' 
-python main.py --config configs/omni/lab/default.txt --stabilize 1
+python main.py --config configs/DyOmni/Lab/default.txt --stabilize 1
 
 # Training - Stage 2 (Color Decomposion) 
 # change the corresponding .txt name
 # For example, if train for indoor scene 'Lab' in werp grids, 
 # please let the 'common_werp_palette_indoor.txt' renamed into 'common.txt' 
-python main.py --config configs/omni/lab/default.txt --palette_train 1
+python main.py --config configs/DyOmni/Lab/default.txt --palette_train 1
 
 # Editing (Inference for recolored videos based on the stage 2's model) 
 # change the corresponding .txt name
@@ -34,10 +34,10 @@ python main.py --config configs/omni/lab/default.txt --palette_train 1
 # please let the 'common_werp_palette_indoor.txt' renamed into 'common.txt'
 # There are different editing options:
 # 1. Recoloring
-python main.py --config configs/omni/lab/default.txt --palette_edit 1 --recolor
+python main.py --config configs/DyOmni/Lab/default.txt --palette_edit 1 --recolor
 # 2. Relighting
-python main.py --config configs/omni/lab/default.txt --palette_edit 1 --relighting
+python main.py --config configs/DyOmni/Lab/default.txt --palette_edit 1 --relighting
 # 3. Retexturing
-python main.py --config configs/omni/lab/default.txt --palette_edit 1 --retexture
+python main.py --config configs/DyOmni/Lab/default.txt --palette_edit 1 --retexture
 # 4. soft and hard segmentation
-python main.py --config configs/omni/lab/default.txt --palette_edit 1 --visualize_seg
+python main.py --config configs/DyOmni/Lab/default.txt --palette_edit 1 --visualize_seg
